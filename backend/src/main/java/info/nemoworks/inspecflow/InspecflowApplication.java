c1b6b734c98c762fb6faadae8447841d8c7bab64
Chun <caochun@gmail.com>
2023-06-04 15:48:31 +0800

    update

---
+++ backend/src/main/java/info/nemoworks/inspecflow/InspecflowApplication.java
@@ -0,0 +1,13 @@
+package info.nemoworks.inspecflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InspecflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InspecflowApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/info/nemoworks/inspecflow/InspecflowApplicationTests.java
@@ -0,0 +1,13 @@
+package info.nemoworks.inspecflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InspecflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
