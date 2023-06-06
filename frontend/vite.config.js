a69e5c214e407a80e004578396b20e1795b72bee
Chun <caochun@gmail.com>
2023-06-06 15:30:29 +0800

    package frontend into backend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -17,19 +17,19 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
-  server: {
-    proxy: {
-        '/api': {
-            target: 'http://localhost:8080',
-            changeOrigin: true,
-            rewrite: (path) => path.replace(/^\/api/, '')
-        },
-        '/bpmn': {
-            target: 'http://localhost:8081',
-            changeOrigin: true,
-            rewrite: (path) => path.replace(/^\/bpmn/, '')
-        }
-    }
   }
+  // server: {
+  //   proxy: {
+  //       '/api': {
+  //           target: 'http://localhost:8080',
+  //           changeOrigin: true,
+  //           rewrite: (path) => path.replace(/^\/api/, '')
+  //       },
+  //       '/bpmn': {
+  //           target: 'http://localhost:8081',
+  //           changeOrigin: true,
+  //           rewrite: (path) => path.replace(/^\/bpmn/, '')
+  //       }
+  //   }
+  // }
 })
